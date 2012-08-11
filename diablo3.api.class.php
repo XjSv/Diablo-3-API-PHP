54f4c77bd89d79b0f1c19a086ec3b7674f0a04eb
Armando Tresova <xjsv24@gmail.com>
2012-08-11 17:42:34 -0400

    usage

--- diablo3.api.class.php
+++ diablo3.api.class.php
@@ -40,11 +40,11 @@ class Diablo3 {
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL,            $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($curl, CURLOPT_TIMEOUT,        30);
         curl_setopt($curl, CURLOPT_MAXREDIRS,      7);
-        curl_setopt($curl, CURLOPT_HEADER,         0);
+        curl_setopt($curl, CURLOPT_HEADER,         false);
 
         $data = curl_exec($curl);
         if($data === false) {
@@ -57,35 +57,35 @@ class Diablo3 {
 
     public function getCareer() {
         $data = $this->getData($this->career_url);
-        return json_decode($data);
+        return json_decode($data, true);
     }
 
     public function getHero($hero_id = null) {
         if($hero_id == null) { return false; }
 
         $data = $this->getData($this->hero_url.$hero_id);
-        return json_decode($data);
+        return json_decode($data, true);
     }
 
     public function getItem($item_data = null) {
         if($item_data == null) { return false; }
 
         $data = $this->getData($this->item_url.$item_data);
-        return json_decode($data);
+        return json_decode($data, true);
     }
 
     public function getFollower($follower_type = null) {
         if($follower_type == null || !in_array($follower_type, $this->followerTypes)) { return false; }
 
         $data = $this->getData($this->follower_url.$follower_type);
-        return json_decode($data);
+        return json_decode($data, true);
     }
 
     public function getArtisan($artisan_type = null) {
         if($artisan_type == null || !in_array($artisan_type, $this->artisanTypes)) { return false; }
 
         $data = $this->getData($this->artisan_url.$artisan_type);
-        return json_decode($data);
+        return json_decode($data, true);
     }
 
     public function __desctruct() {
--- usage.php
+++ usage.php
@@ -1,12 +1,16 @@
 <?php
 require_once('diablo3.api.class.php');
 
-$Diablo3 = new Diablo3("XjSv-1677");
+$Diablo3 = new Diablo3("XjSv-1677"); // Battle.net Tag
 
-$CAREER_DATA     = $Diablo3->getCareer();
-//$HERO_DATA     = $Diablo3->getHero(3982160);
-//$ITEM_DATA     = $Diablo3->getItem('COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD');
-//$FOLLOWER_DATA = $Diablo3->getFollower('enchantress');
-//$ARTISAN_DATA  = $Diablo3->getArtisan('blacksmith');
+$CAREER_DATA   = $Diablo3->getCareer();
+$HERO_DATA     = $Diablo3->getHero(3982160);  // Hero ID
+$ITEM_DATA     = $Diablo3->getItem('COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD'); // Item Data
+$FOLLOWER_DATA = $Diablo3->getFollower('enchantress'); // Options: 'enchantress', 'templar', 'scoundrel'
+$ARTISAN_DATA  = $Diablo3->getArtisan('blacksmith');   // Options: 'blacksmith', 'jeweler'
 
 print_r($CAREER_DATA);
+print_r($HERO_DATA);
+print_r($ITEM_DATA);
+print_r($FOLLOWER_DATA);
+print_r($ARTISAN_DATA);
