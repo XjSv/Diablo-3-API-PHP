990af451af4aac96195864d2520d3cf58cba7a1b
Armando Tresova <xjsv24@gmail.com>
2012-08-18 03:21:26 -0300

    Update diablo3.api.class.php
    Always return a string back and handle some of the errors returned.
    
    This fixes #2

--- diablo3.api.class.php
+++ diablo3.api.class.php
@@ -20,10 +20,10 @@ class Diablo3 {
 
             //  Check if its a valid Battle.net tag (Pending)
             //
-            if(!preg_match('/^.+(-[0-9]{4})/', $battlenet_tag)) {
+            /*if(!preg_match('/^.+(-[0-9]{4})/', $battlenet_tag)) {
                 error_log("Battle.net tag provided not valid.");
-                //exit(0); // TODO: This is currently broken
-            }
+                exit(0);
+            }*/
 
             //  Set Variables
             //
@@ -49,8 +49,11 @@ class Diablo3 {
     }
 
     private function getData($url) {
-        if(!$this->cURLcheckBasics()) return false;
         if($url == '') return false;
+        if(!$this->cURLcheckBasics()) {
+            error_log("cURL is NOT Available");
+            return false;
+        }
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL,            $url);
@@ -61,45 +64,84 @@ class Diablo3 {
         curl_setopt($curl, CURLOPT_HEADER,         false);
 
         $data = curl_exec($curl);
-        if($data === false) {
-            $data = curl_error($curl);
-        }
         curl_close($curl);
+
+        // Debug
+        //
+        //error_log("URL: ".$url);
+        //error_log("Data: ".$data);
+
+        $error = strpos($data, 'Error report');
+        if($error) {
+            $data = false;
+        } else {
+            $data = json_decode($data, true);
+        }
+
+        if(isset($data['code']) && ($data['code'] == 'OOPS' || $data['code'] == 'LIMITED')) {
+            error_log('API Fail Reason: '.$data['reason']);
+            $data = false;
+        }
 
         return $data;
     }
 
     public function getCareer() {
         $data = $this->getData($this->career_url);
-        return json_decode($data, true);
+
+        if($data) {
+            return $data;
+        } else {
+            return 'No Data Return';
+        }
     }
 
     public function getHero($hero_id = null) {
-        if($hero_id == null) return false;
+        if($hero_id == null || !preg_match('/^[0-9]+$/', $hero_id)) return 'Invalid/Empty Hero Id';
 
         $data = $this->getData($this->hero_url.$hero_id);
-        return json_decode($data, true);
+
+        if($data) {
+            return $data;
+        } else {
+            return 'No Data Return';
+        }
     }
 
     public function getItem($item_data = null) {
-        if($item_data == null) return false;
+        if($item_data == null || !preg_match('/^[a-zA-Z0-9]+$/', $item_data)) return 'Invalid/Empty Item Data';
 
         $data = $this->getData($this->item_url.$item_data);
-        return json_decode($data, true);
+
+        if($data) {
+            return $data;
+        } else {
+            return 'No Data Return';
+        }
     }
 
     public function getFollower($follower_type = null) {
-        if($follower_type == null || !in_array($follower_type, $this->followerTypes)) return false;
+        if($follower_type == null || !in_array($follower_type, $this->followerTypes)) return 'Invalid/Empty Follower Type';
 
         $data = $this->getData($this->follower_url.$follower_type);
-        return json_decode($data, true);
+
+        if($data) {
+            return $data;
+        } else {
+            return 'No Data Return';
+        }
     }
 
     public function getArtisan($artisan_type = null) {
-        if($artisan_type == null || !in_array($artisan_type, $this->artisanTypes)) return false;
+        if($artisan_type == null || !in_array($artisan_type, $this->artisanTypes)) return 'Invalid/Empty Artisan Type';
 
         $data = $this->getData($this->artisan_url.$artisan_type);
-        return json_decode($data, true);
+
+        if($data) {
+            return $data;
+        } else {
+            return 'No Data Return';
+        }
     }
 
     public function __desctruct() {
