212f886e3a824063182f93f20b27645e167297f3
Armando Tresova <xjsv24@gmail.com>
2012-08-19 02:47:45 -0300

    Update usage.php

--- usage.php
+++ usage.php
@@ -1,13 +1,12 @@
 <?php
 require_once('diablo3.api.class.php');
 
-$Diablo3 = new Diablo3("XjSv#1677"); // Battle.net Tag
-
+$Diablo3       = new Diablo3("XjSv#1677");                                                      // Battle.net Tag ID. 'XjSv#1677' or 'XjSv-1677' (string)
 $CAREER_DATA   = $Diablo3->getCareer();
-$HERO_DATA     = $Diablo3->getHero(3982160);  // Hero ID
-$ITEM_DATA     = $Diablo3->getItem('COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD'); // Item Data
-$FOLLOWER_DATA = $Diablo3->getFollower('enchantress'); // Options: 'enchantress', 'templar', 'scoundrel'
-$ARTISAN_DATA  = $Diablo3->getArtisan('blacksmith');   // Options: 'blacksmith', 'jeweler'
+$HERO_DATA     = $Diablo3->getHero(3982160);                                                    // Hero ID (int)
+$ITEM_DATA     = $Diablo3->getItem('COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD'); // Item Data 'COGHsoAIEgcIBBXIGEoRHYQRdRUdnWyzFB2qXu51MA04kwNAAFAKYJMD'  (string)
+$FOLLOWER_DATA = $Diablo3->getFollower('templar');                                              // Options: 'enchantress', 'templar', 'scoundrel' (string)
+$ARTISAN_DATA  = $Diablo3->getArtisan('blacksmith');                                            // Options: 'blacksmith', 'jeweler' (string)
 
 print_r($CAREER_DATA);
 print_r($HERO_DATA);
