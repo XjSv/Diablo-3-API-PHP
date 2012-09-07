284fa73b942cb2dcbac38e4d4d558e30315ad23a
Armando Tresova <xjsv24@gmail.com>
2012-09-07 01:25:01 -0300

    Added missing function.

--- functions.php
+++ functions.php
@@ -1,4 +1,26 @@
 <?php
+function secondsToTime($seconds) {
+    // extract hours
+    $hours = floor($seconds / (60 * 60));
+
+    // extract minutes
+    $divisor_for_minutes = $seconds % (60 * 60);
+    $minutes             = floor($divisor_for_minutes / 60);
+
+    // extract the remaining seconds
+    $divisor_for_seconds = $divisor_for_minutes % 60;
+    $seconds             = ceil($divisor_for_seconds);
+
+    // return the final array
+    $obj = array("h" => (int)$hours,
+                 "m" => (int)$minutes,
+                 "s" => (int)$seconds);
+
+    $time = implode(':', $obj);
+
+    return $time;
+}
+
 function getAllTheData($battlenet_tags = array(), $server = 'us') {
     if(!(count($battlenet_tags) > 0)) return false;
     if(!in_array($server, array('us', 'eu', 'sea'), true)) $server = 'us';
