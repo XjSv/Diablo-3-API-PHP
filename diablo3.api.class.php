d41c3b7d8c125b1535406998067cc1e2ac1c6fd7
Armando Tresova <xjsv24@gmail.com>
2012-11-25 21:25:31 -0500

    Update diablo3.api.class.php
    Fixed file get

--- diablo3.api.class.php
+++ diablo3.api.class.php
@@ -166,7 +166,7 @@ class Diablo3 {
                 $fp   = fopen($real_item_path.$size.$icon.$ext, 'wb');
                 $curl = curl_init();
                 curl_setopt($curl, CURLOPT_URL,            $url);
-                curl_setopt($curl, CURLOPT_URL,            $url);
+                curl_setopt($curl, CURLOPT_FILE,           $fp);
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
                 curl_setopt($curl, CURLOPT_FILE,           $fp);
